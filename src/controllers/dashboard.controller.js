e4004a57cdd7da236e43607135a881e38584ed4c
Fahad1bashir2 <fahad85581@gmail.com>
2026-06-11 10:32:54 +0500

    update  dashboard and health controllers

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,20 +1,72 @@
-import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
 // import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Subscription } from "../models/subscription.model.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+  const videosResult = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalVideos: { $sum: 1 },
+        totalViews: { $sum: "$views" },
+      },
+    },
+  ]);
+  const totalVideos = videosResult[0]?.totalVideos || 0;
+  const totalViews = videosResult[0]?.totalViews || 0;
+
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: req.user._id,
+  });
+  const result = await Like.aggregate([
+    {
+      $lookup: {
+        from: "videos",
+        foreignField: "_id",
+        localField: "video",
+        as: "videoInfo",
+      },
+    },
+    { $unwind: "$videoInfo" },
+    {
+      $match: {
+        "videoInfo.owner": new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $count: "totalLikes",
+    },
+  ]);
+  const totalLikes = result[0]?.totalLikes || 0;
+
+  const stats = {
+    totalVideos,
+    totalViews,
+    totalSubscribers,
+    totalLikes,
+  };
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, stats, "User data fetched Successfully"));
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
+  const videos = await Video.find({ owner: req.user._id })
+    .sort({ createdAt: -1 })
+    .select("title description videoFile thumbnail views createdAt");
 
-export {
-    getChannelStats, 
-    getChannelVideos
-    }
+  return res.status(200).json(new ApiResponse(200, videos, "All Videos for this channel fetched"));
+});
+
+export { getChannelStats, getChannelVideos };
--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,5 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const healthCheck = asyncHandler(async (req, res) => {});
+const healthCheck = asyncHandler(async (req, res) => {
+    res.status(200).json({ status: "OK", timestamp: new Date() });
+});
 
 export {healthCheck}
